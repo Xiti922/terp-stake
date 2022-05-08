866cf0edae4eb8e413addf505bd0e91e3e22faa7
Xiti922 <100534270+Xiti922@users.noreply.github.com>
2022-05-08 13:24:47 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,47 +1,22 @@
 export const config = {
-    RPC_URL: "https://rpc-chronic.zenchainlabs.io/",
-    REST_URL: "https://api-chronic.zenchainlabs.io/",
-    CHAIN_ID: "morocco-1",
-    CHAIN_NAME: "Chronic-Token",
+    RPC_URL: 'https://rpc-chronic.zenchainlabs.io',
+    REST_URL: 'https://api-chronic.zenchainlabs.io',
+    EXPLORER_URL: 'https://chronic.zenscan.io',
+    STAKING_URL: 'https://try-2.vercel.app/stake',
+    NETWORK_NAME: 'Chronic',
+    NETWORK_TYPE: 'mainnet',
+    CHAIN_ID: 'morocco-1',
+    CHAIN_NAME: 'Morocco-1',
     stakeCurrency: {
       COIN_DENOM: "CHT",
       COIN_MINIMAL_DENOM: "ucht",
       COIN_DECIMALS: 6,
     },
-    bip44: {
-      COIN_TYPE: 118,
-    },
-     "bech32Config": {
-        "bech32PrefixAccAddr": "chronic",
-        "bech32PrefixAccPub": "chronicpub",
-        "bech32PrefixValAddr": "chronicvaloper",
-        "bech32PrefixValPub": "chronicvaloperpub",
-        "bech32PrefixConsAddr": "chronicvalcons",
-        "bech32PrefixConsPub": "chronicvalconspub"
-    },
-    currencies: [
-      {
-        COIN_DENOM: "CHT",
-        COIN_MINIMAL_DENOM: "ucht",
-        COIN_DECIMALS: 6,
-      },
-      {
-        COIN_DENOM: "CGAS",
-        COIN_MINIMAL_DENOM: "ucgas",
-        COIN_DECIMALS: 6,
-      },
-    ],
-    feeCurrencies: [
-      {
-        COIN_DENOM: "CGAS",
-        COIN_MINIMAL_DENOM: "ucgas",
-        COIN_DECIMALS: 6,
-      },
-    ],
-    gasPriceStep: {
-      GAS_PRICE_STEP_LOW: 0.001,
-      GAS_PRICE_STEP_AVERAGE: 0.0025,
-      GAS_PRICE_STEP_HIGH: 0.003,
-    },
-    features: ['ibc-go', 'ibc-transfer'],
-   };
+    PREFIX: 'chronic',
+    COIN_TYPE: 118,
+    COINGECKO_ID: 'chronic',
+    GAS_PRICE_STEP_LOW: 0.001,
+    GAS_PRICE_STEP_AVERAGE: 0.01,
+    GAS_PRICE_STEP_HIGH: 0.05,
+    FEATURES: ['ibc-transfer', 'ibc-go'],
+};
