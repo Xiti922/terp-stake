99ab687499e38feae1ce3f5f187d4ecec07d5b7f
Xiti922 <100534270+Xiti922@users.noreply.github.com>
2022-05-08 13:19:14 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 export const config = {
     RPC_URL: "https://rpc-chronic.zenchainlabs.io/",
     REST_URL: "https://api-chronic.zenchainlabs.io/",
-    chainId: "morocco-1",
-    chainName: "Chronic-Token",
+    CHAIN_ID: "morocco-1",
+    CHAIN_NAME: "Chronic-Token",
     stakeCurrency: {
-      coinDenom: "CHT",
-      coinMinimalDenom: "ucht",
-      coinDecimals: 6,
+      COIN_DENOM: "CHT",
+      COIN_MINIMAL_DENOM: "ucht",
+      COIN_DECIMALS: 6,
     },
     bip44: {
-      coinType: 118,
+      COIN_TYPE: 118,
     },
      "bech32Config": {
         "bech32PrefixAccAddr": "chronic",
@@ -21,27 +21,27 @@ export const config = {
     },
     currencies: [
       {
-        coinDenom: "CHT",
-        coinMinimalDenom: "ucht",
-        coinDecimals: 6,
+        COIN_DENOM: "CHT",
+        COIN_MINIMAL_DENOM: "ucht",
+        COIN_DECIMALS: 6,
       },
       {
-        coinDenom: "CGAS",
-        coinMinimalDenom: "ucgas",
-        coinDecimals: 6,
+        COIN_DENOM: "CGAS",
+        COIN_MINIMAL_DENOM: "ucgas",
+        COIN_DECIMALS: 6,
       },
     ],
     feeCurrencies: [
       {
-        coinDenom: "CGAS",
-        coinMinimalDenom: "ucgas",
-        coinDecimals: 6,
+        COIN_DENOM: "CGAS",
+        COIN_MINIMAL_DENOM: "ucgas",
+        COIN_DECIMALS: 6,
       },
     ],
     gasPriceStep: {
-      low: 0.001,
-      average: 0.0025,
-      high: 0.003,
+      GAS_PRICE_STEP_LOW: 0.001,
+      GAS_PRICE_STEP_AVERAGE: 0.0025,
+      GAS_PRICE_STEP_HIGH: 0.003,
     },
-    features: ["ibc-go"],
+    features: ['ibc-go', 'ibc-transfer'],
    };
