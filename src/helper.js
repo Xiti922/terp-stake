8075047cad3e8c3d230a048ab5c6c8daaf23eb5e
Xiti922 <100534270+Xiti922@users.noreply.github.com>
2022-05-08 13:00:16 +0200

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -9,7 +9,6 @@ const chainName = config.CHAIN_NAME;
 const coinDenom = config.COIN_DENOM;
 const coinMinimalDenom = config.COIN_MINIMAL_DENOM;
 const coinDecimals = config.COIN_DECIMALS;
-const feeCurrencies = config.FEE_CURRENCY;
 const prefix = config.PREFIX;
 const coinGeckoId = config.COINGECKO_ID;
 
